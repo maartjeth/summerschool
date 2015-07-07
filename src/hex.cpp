ee67e80aaf64d18cfd9d271ed8847e66c55e900f
Gloomy-Mammoth <Gloomy-Mammoth@users.noreply.github.com>
2015-07-07 11:22:23 +0200

    Print board from array

--- src/hex.cpp
+++ src/hex.cpp
@@ -1,21 +1,38 @@
 #include <iostream>
-#include <vector>
+#include <array>
 
 class hexBoard
+/* Initialize a board for the game Hex. */
 {
 public:
-    char line1[5] = {'*',' ','*',' ','*'};
-    char line2[5] = {'  ','*',' ','*',' '};
-
-    void print_board()
+    void InitBoard(int nRows, int nCols)
     {
-        for(int i = 0; i < 6; i++)
+        char board[nRows][nCols];
+
+        for (int i = 0; i < nRows; i++)
         {
-            std::cout << line1 << std::endl << line2 << std::endl << line1;
+            for (int j = 0; j < nCols; j++)
+            {
+                board[i][j] = '_';
+            }
         }
-        std::cout << std::endl;
+        for (int k = 0; k < nRows; k++)
+        {
+            for (int m = 0; m < nCols; m++)
+            {
+                std::cout << board[k][m];
+            }
+            std::cout << std::endl;
+        }
     }
 };
+
+/* Initialize the Hex board with default size 11 x 11. */
+// hexBoard::PrintBoard()
+// {
+//     nRows = 11;
+//     nCols = 11;
+// }
 
 class chooseLocation
 {
@@ -33,6 +50,6 @@ public:
 int main()
 {
     hexBoard board;
-    board.print_board();
+    board.InitBoard(11,11);
 
 }
