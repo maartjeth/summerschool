d464d970a505a2489721b4bad14df6fe03b16164
Gloomy-Mammoth <Gloomy-Mammoth@users.noreply.github.com>
2015-07-08 18:14:59 +0200

    Expand valid neighbour function and isVisited and cleanup

--- pathfinding/pathfinding.cpp
+++ pathfinding/pathfinding.cpp
@@ -18,12 +18,17 @@ public:
     // Stack of nodes still to visit.
     std::stack<int> nodes;
 
-    bool isVisited()
+    bool isVisited(int position)
+    // Check whether a position ahs been visited.
     {
-
+        if (find(visited.begin(), visited.end(), position) == visited.end())
+        {
+            return true;
+        }
+        return false;
     }
 
-    bool isValidNeighbour(int position, int player, std::list<int> visitednode, int nCols, int nRows)
+    bool isValidNeighbour(int position, int player, int nCols, int nRows, int arr [])
     {
         // Number of array elements.
         int nArrElements;
@@ -34,7 +39,15 @@ public:
         {
             if (position >= 0)
             {
-                return true;
+                // Check whether the position has already been visited.
+                if (isVisited(position))
+                {
+                    // Check whether the position is occupied by the current player.
+                    if (arr[position] == player)
+                    {
+                        return true;
+                    }
+                }
             }
         }
         return false;
@@ -58,11 +71,11 @@ public:
             nodes.pop();
             std::cout << top << std::endl;
             // If the top has not been visited.
-            if (find( visited.begin(), visited.end(), top) == visited.end())
+            if (isVisited(top))
             {
                 // If we have come to the edge of the graph.
                 // TODO:FIX THE FINAL VALUE.
-                if ((top == 24) && (arr[top] == 1))
+                if (top == 120)
                 {
                     std::cout << "A path was found." << std::endl;
                     break;
@@ -73,28 +86,27 @@ public:
                     visited.insert(visited.end(), top);
 
                     // Add valid neighbours of top to the stack.
-                    // TODO: MAKE SURE ALREADY VISITED NODES ARE NOT READDED.
-                    if (isValidNeighbour(top-1, player, visited, nCols, nRow))
+                    if (isValidNeighbour(top-1, player, nCols, nRow, arr))
                     {
                         nodes.push(top-1);
                     }
-                    if (isValidNeighbour(top+1, player, visited, nCols, nRow))
+                    if (isValidNeighbour(top+1, player, nCols, nRow, arr))
                     {
                         nodes.push(top+1);
                     }
-                    if (isValidNeighbour(top+nCols, player, visited, nCols, nRow))
+                    if (isValidNeighbour(top+nCols, player, nCols, nRow, arr))
                     {
                         nodes.push(top+nCols);
                     }
-                    if (isValidNeighbour(top + nCols - 1, player, visited, nCols, nRow))
+                    if (isValidNeighbour(top + nCols - 1, player, nCols, nRow, arr))
                     {
                         nodes.push(top + nCols - 1);
                     }
-                    if (isValidNeighbour(top - nCols, player, visited, nCols, nRow))
+                    if (isValidNeighbour(top - nCols, player, nCols, nRow, arr))
                     {
                         nodes.push(top - nCols);
                     }
-                    if (isValidNeighbour(top - nCols + 1, player, visited, nCols, nRow))
+                    if (isValidNeighbour(top - nCols + 1, player, nCols, nRow, arr))
                     {
                         nodes.push(top - nCols + 1);
                     }
@@ -107,29 +119,27 @@ public:
 
 int main()
 {
-    // intArray board;
-    // board.popArray();
     int nRows, nCols;
-    // nRows = 11;
-    // nCols = 11;
-    // int boardarray [] = {  1, -1, 0, 1, 1, 1, -1, 0, 1, 1, 1,
-    //                         1, 1, -1, 0, 1, 1, -1, 0, 1, 1, 1,
-    //                         1, 1, 0, 1, 1, 1, -1, 0, 1, 1, 1,
-    //                         1, 0, -1, 1, 1, 1, -1, 0, 1, 1, 1,
-    //                         1, 1, 1, 1, 1, 1, -1, 0, 1, 1, 1,
-    //                         1, -1, 0, 1, 1, 1, -1, 0, 1, 1, 1,
-    //                         1, 1, -1, 0, 1, 1, -1, 0, 1, 1, 1,
-    //                         1, 1, 0, 1, 1, 1, -1, 0, 1, 1, 1,
-    //                         1, 0, -1, 1, 1, 1, 1, 1, 1, 1, 1,
-    //                         1, 1, 1, 1, 1, 1, -1, 0, 1, 1, 1,
-    //                         1, 1, 1, 1, 1, 1, -1, 0, 1, 1, 1};
-    nRows = 5;
-    nCols = 5;
-    int boardarray [] = {1,0,0,0,0,
-                        1,1,1,0,0,
-                        0,0,1,1,1,
-                        0,0,0,0,1,
-                        0,0,0,0,1};
+    nRows = 11;
+    nCols = 11;
+    int boardarray [] = {  1, -1, 0, 1, 1, 1, -1, 0, 1, 1, 1,
+                            1, 1, -1, 0, 1, 1, -1, 0, 1, 1, 1,
+                            1, 1, 0, 1, 1, 1, -1, 0, 1, 1, 1,
+                            1, 0, -1, 1, 1, 1, -1, 0, 1, 1, 1,
+                            1, 1, 1, 1, 1, 1, -1, 0, 1, 1, 1,
+                            1, -1, 0, 1, 1, 1, -1, 0, 1, 1, 1,
+                            1, 1, -1, 0, 1, 1, -1, 0, 1, 1, 1,
+                            1, 1, 0, 1, 1, 1, -1, 0, 1, 1, 1,
+                            1, 0, -1, 1, 1, 1, 1, 1, 1, 1, 1,
+                            1, 1, 1, 1, 1, 1, -1, 0, 1, 1, 1,
+                            1, 1, 1, 1, 1, 1, -1, 0, 1, 1, 1};
+    // nRows = 5;
+    // nCols = 5;
+    // int boardarray [] = {1,0,0,0,0,
+    //                     1,1,1,0,0,
+    //                     0,0,1,1,1,
+    //                     0,0,0,0,1,
+    //                     0,0,0,0,1};
 
     pathfinding finder;
     finder.dfs(boardarray, 0, nRows, nCols);
